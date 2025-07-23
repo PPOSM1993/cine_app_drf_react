351516b5427891a1ec786826c21db67219facaf7
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-07-23 17:41:23 -0400

    Se hacen los Cruds de las Apps movies y authentication

--- backend/apps/movies/views.py
+++ backend/apps/movies/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters, generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Movie
+from .serializers import MovieSerializer
+from utils.pagination import StandardResultsSetPagination
+from django.db.models import Q
 
-# Create your views here.
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    """
+    CRUD completo para películas.
+    """
+    #queryset = Movie.objects.all().order_by('-release_date')
+    queryset = Movie.objects.prefetch_related('genres', 'formats').order_by('-release_date')
+
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination
+
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    #filterset_fields = ['classification', 'is_active', 'is_scheduled', 'release_date']
+    filterset_fields = ['classification', 'is_active', 'is_scheduled', 'release_date', 'genres', 'formats']
+
+    search_fields = ['title', 'director', 'main_cast', 'production_company', 'original_language']
+    ordering_fields = ['release_date', 'title', 'expected_audience']
+
+
+class MovieSearchView(generics.ListAPIView):
+    """
+    Vista especializada para búsqueda libre.
+    """
+    #queryset = Movie.objects.filter(is_active=True)
+    queryset = Movie.objects.prefetch_related('genres', 'formats').filter(is_active=True)
+
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = StandardResultsSetPagination  # opcional si quieres paginar resultados de búsqueda también
+
+    def get_queryset(self):
+        """
+        Permite búsqueda por título, director, reparto, productora.
+        """
+        queryset = self.queryset
+        q = self.request.query_params.get('q', '').strip()
+        if q:
+            queryset = queryset.filter(
+                Q(title__icontains=q) |
+                Q(director__icontains=q) |
+                Q(main_cast__icontains=q) |
+                Q(production_company__icontains=q)
+            )
+
+        return queryset.order_by('-release_date')
